a8907768fa9cd1efacb0f1f75518d99d84f0b978
Frederik Hvilshøj <frederik@cord.tech>
2024-10-11 11:09:07 +0200

    fix url parsing for test command

--- encord_agents/cli/test.py
+++ encord_agents/cli/test.py
@@ -38,15 +38,30 @@ def local(
         "frame": [green]frame[/green] or 0
     }
     """
+    import re
     from pprint import pprint
 
     import requests
 
-    splits = url.split("/")
-    idx = splits.index("label_editor")
-    project_hash, data_hash, *rest = splits[idx + 1 :]
-    frame = int(rest[0]) if rest else 0
-    payload = {"projectHash": project_hash, "dataHash": data_hash, "frame": frame}
+    parts_regex = r"https:\/\/app.encord.com\/label_editor\/(?P<projectHash>.*?)\/(?P<dataHash>[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12})(/(?P<frame>\d+))?\??"
+    editor_url = "https://app.encord.com/label_editor/a1240d34-e4e7-4f9c-b62c-d09b3dfd010e/5f68f13e-3931-4585-ad80-e15dd2c501b5/0"  # @param { type : "string" }
+
+    # @markdown Upon a 200 response, refresh the label editor to see the result.
+
+    try:
+        payload = re.match(parts_regex, editor_url).groupdict()
+        payload["frame"] = payload["frame"] or 0
+    except:
+        import sys
+
+        import typer
+
+        print(
+            "Was not able to parse the url. Please make sure that it follows the format 'label_editor/project_hash/data_hash/frame'",
+            file=sys.stderr,
+        )
+        raise typer.Abort()
+
     response = requests.post(
         f"http://localhost:{port}/{target}",
         json=payload,
