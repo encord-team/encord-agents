1ff4da86e606c22c0eda63711bc5efd15faa830e
Frederik Hvilshøj <frederik@cord.tech>
2024-11-04 13:25:28 +0100

    feat(cli): add option to print project agent nodes for convenience

--- encord_agents/cli/print.py
+++ encord_agents/cli/print.py
@@ -1,6 +1,9 @@
 import sys
 
+from encord.orm.workflow import WorkflowStageType
 from typer import Typer
+
+from encord_agents.core.settings import Settings
 
 app = Typer(
     name="print",
@@ -8,6 +11,42 @@ app = Typer(
     rich_markup_mode="rich",
     no_args_is_help=True,
 )
+
+
+@app.command(name="agent-nodes")
+def print_agent_nodes(project_hash: str):
+    """
+    Prints agent nodes from project.
+
+    Given the project hash, loads the project and prints the agent nodes.
+
+    Args:
+        project_hash: The project hash for which to print agent nodes.
+
+    """
+    import rich
+    from encord.exceptions import AuthorisationError
+    from encord.user_client import EncordUserClient
+
+    _ = Settings()
+    client = EncordUserClient.create_with_ssh_private_key()
+    try:
+        project = client.get_project(project_hash)
+    except AuthorisationError:
+        rich.print(f"You do not seem to have access to project with project hash `[purple]{project_hash}[/purple]`")
+        exit()
+
+    agent_nodes = [
+        f'AgentStage(title="{n.title}", uuid="{n.uuid}")'
+        for n in project.workflow.stages
+        if n.stage_type == WorkflowStageType.AGENT
+    ]
+    if not agent_nodes:
+        print("Project does not have any agent nodes.")
+        return
+
+    for node in agent_nodes:
+        rich.print(node)
 
 
 @app.command(name="system-info")
