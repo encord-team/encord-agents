a1deb60459820949110e41fd4326dc81dccb8a22
Frederik Hvilshøj <frederik@cord.tech>
2024-11-18 14:03:24 +0100

    fix: better settings parsing and let printable error just take care of formatting on its own

--- encord_agents/core/settings.py
+++ encord_agents/core/settings.py
@@ -11,7 +11,7 @@ from typing import Optional
 from pydantic import Field, field_validator, model_validator
 from pydantic_settings import BaseSettings
 
-from encord_agents.exceptions import PrintableError, format_printable_error
+from encord_agents.exceptions import PrintableError
 
 
 class Settings(BaseSettings):
@@ -33,6 +33,21 @@ class Settings(BaseSettings):
     """
     domain: Optional[str] = Field(validation_alias="ENCORD_DOMAIN", default=None)
 
+    @field_validator("ssh_key_content")
+    @classmethod
+    def check_key_content(cls, content: str | None):
+        if content is None:
+            return content
+
+        file_attempt = Path(content).expanduser()
+        if file_attempt.is_file():
+            raise PrintableError(
+                f"The env variable `[blue]ENCORD_SSH_KEY[/blue]` (={file_attempt}) is set with a value that looks like a path and not ssh key content. Did you mean to set the `[blue]ENCORD_SSH_KEY_FILE[/blue]` environment variable with the private key file content directly?"
+            )
+
+        return content
+
+
     @field_validator("ssh_key_file")
     @classmethod
     def check_path_expand_and_exists(cls, path: Path | None):
@@ -40,17 +55,25 @@ class Settings(BaseSettings):
             return path
 
         path = path.expanduser()
-        assert path.is_file(), f"Provided ssh key file (ENCORD_SSH_KEY_FILE: '{path}') does not exist"
+
+        if not path.is_file():
+            raise PrintableError(
+                "The env variable `[blue]ENCORD_SSH_KEY_FILE[/blue]` is set with a value that could not be found in the file system. Did you mean to set the `[blue]ENCORD_SSH_KEY[/blue]` environment variable with the private key file content directly?"
+            )
+
         return path
 
     @model_validator(mode="after")
-    @format_printable_error
     def check_key(self):
         if not any(
             map(bool, [self.ssh_key_content, self.ssh_key_file])
         ): 
             raise PrintableError(f"Must specify either `[blue]ENCORD_SSH_KEY_FILE[/blue]` or `[blue]ENCORD_SSH_KEY[/blue]` env variables. If you don't have an ssh key, please refere to our docs:{os.linesep}[magenta]https://docs.encord.com/platform-documentation/Annotate/annotate-api-keys#creating-keys-using-terminal-powershell[/magenta]")
-        # TODO help people find their way through ssh keys
+
+        if all(map(bool, [self.ssh_key_file, self.ssh_key_content])):
+            import warnings
+            warnings.warn("You have configured both the `ENCORD_SSH_KEY` and `ENCORD_SSH_KEY_FILE`. The `ENCORD_SSH_KEY` will take precedence.")
+
         return self
 
     @property
