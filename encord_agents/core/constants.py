4ecd9986181ccdd0882e2516a53af42619a75279
Frederik Hvilshøj <frederik@cord.tech>
2024-12-20 09:14:12 +0100

    fix: cors domain regex account for us deployment

--- encord_agents/core/constants.py
+++ encord_agents/core/constants.py
@@ -1,3 +1,3 @@
 ENCORD_DOMAIN_REGEX = (
-    r"^https:\/\/(?:(?:cord-ai-development--[\w\d]+-[\w\d]+\.web.app)|(?:(?:dev|staging|app)\.encord\.com))$"
+    r"^https:\/\/(?:(?:cord-ai-development--[\w\d]+-[\w\d]+\.web.app)|(?:(?:dev|staging|app)\.(us\.)?encord\.com))$"
 )
