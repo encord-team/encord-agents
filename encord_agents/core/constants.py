7763dbf4ce0a269b7b109aa9a0f998f6f11c9e68
Jim Broadbent <jim@encord.com>
2025-03-13 13:07:56 +0000

    Rads Suggestion

--- encord_agents/core/constants.py
+++ encord_agents/core/constants.py
@@ -5,7 +5,7 @@ ENCORD_DOMAIN_REGEX = (
 EDITOR_URL_PARTS_REGEX = r"(?P<domain>https:\/\/app.(us\.)?encord.com)\/label_editor\/(?P<projectHash>.*?)\/(?P<dataHash>[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12})(\/(?P<frame>\d+))?\??"
 
 TEST_REQUEST_PAYLOAD: dict[str, str | int] = {
-    "projectHash": "12345678-1234-5678-1234-123456789012",
-    "dataHash": "abcdefab-abcd-abcd-abcd-abcdefabcdef",
+    "projectHash": "00000000-0000-0000-0000-000000000000",
+    "dataHash": "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
     "frame": 10,
 }
