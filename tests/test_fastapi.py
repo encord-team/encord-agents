1b355503877dd22a1c282d9374b2ec75faccdc0e
Jim Broadbent <jim@encord.com>
2025-05-22 16:09:26 +0100

    tests: FastAPI coverage that headers are correct for non 200 response

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 
+from cv2 import meanShift
 from encord.user_client import EncordUserClient
 from fastapi import Depends
 from fastapi.testclient import TestClient
 
+from encord_agents.core.exceptions import EncordEditorAgentException
 from encord_agents.fastapi.cors import get_encord_app
 from encord_agents.fastapi.dependencies import dep_client
 
@@ -32,6 +34,10 @@ class TestCustomCorsRegex:
         def post_client(client: Annotated[EncordUserClient, Depends(dep_client)]) -> None:
             assert isinstance(client, EncordUserClient)
 
+        @app.post("/client-throws")
+        def post_client_throws(client: Annotated[EncordUserClient, Depends(dep_client)]) -> None:
+            raise EncordEditorAgentException(message="Error message")
+
         client = TestClient(app)
         resp = client.post("/client", headers={"Origin": "https://app.encord.com"})
         assert resp.status_code == 200, resp.content
@@ -40,3 +46,8 @@ class TestCustomCorsRegex:
         resp = client.post("/client", headers={"Origin": "https://example.com"})
         assert resp.status_code == 200, resp.content
         assert "Access-Control-Allow-Origin" not in resp.headers
+
+        # Check that even when we are returning a non 200, we have the appropriate headers on the response
+        resp = client.post("/client-throws", headers={"Origin": "https://app.encord.com"})
+        assert resp.status_code == 400, resp.content
+        assert resp.headers["Access-Control-Allow-Origin"] == "https://app.encord.com"
