3391ca4eaca19af47c5170261c0672bf9674d264
Jim-Encord <156204297+Jim-Encord@users.noreply.github.com>
2025-07-17 13:12:47 +0100

    fix: Fix AWS Imports (#179)
    * tests: Ensure that we at least import GCP and AWS pathways
    
    * fix: Fix AWS import

--- encord_agents/aws/__init__.py
+++ encord_agents/aws/__init__.py
@@ -1,7 +1,6 @@
-from core.exceptions import EncordEditorAgentException
-
 from encord_agents.core.dependencies.models import Depends
 
+from ..core.exceptions import EncordEditorAgentException
 from .wrappers import editor_agent
 
 __all__ = ["editor_agent", "Depends", "EncordEditorAgentException"]
